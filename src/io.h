ae529da391e4fce70787ea7a4a6a2c9aea6c767b
André Taiar <taiar@hotmail.com>
2010-08-15 23:13:44 -0300

    Organizando e comentando o IO

--- src/io.h
+++ src/io.h
@@ -2,11 +2,34 @@
 #include <stdio.h>
 
 #ifndef __io__
-  #define __io__
+#define __io__
 
-  int is_char(char);
-  int is_space(char);
-  int is_line_break(char);
-  void get_token(FILE*, char*, int*);
+/**
+ * Verifica se o caractere passado é valido segundo criterios arbitrarios para leitura do arquivo.
+ */
+int is_char(char);
+
+/**
+ * Verifica se o caractere passado como parâmetro é um espaço.
+ */
+int is_space(char);
+
+/**
+ * Verifica se o caractere passado como parâmetro é uma quebra de linha.
+ */
+int is_line_break(char);
+
+/**
+ * Transforma caracteres de letras maiúsculas em minúsculas para fins de simplificação.
+ */
+char lowerCase(char);
+
+/**
+ * Lê e armazena em uma variável uma string retirada do arquivo de passado como
+ * parâmetro. A string é definida arbitráriamente segundo especificação.
+ * Sinaliza um flag inteiro interpretando o valor char da próxima leitura para
+ * facilitar o trabalho na leitura dos arquivos.
+ */
+void get_token(FILE*, char*, int*);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -5,34 +5,7 @@
 
 int main(int argc, char *argv[])
 {
-  FILE *ent;
-  int flag = 0;
-  char buff[30];
-  char buff2[1024];
-  ent = fopen("../src/e1", "r");
 
-  get_token(ent, buff, &flag);
-  printf("%s\n", buff);
 
-  while(flag != 2)
-  {
-   get_token(ent, buff, &flag);
-   printf("%s\n", buff);
-  }
-
-  fclose(ent);
-  printf("==========================================\n");
-  printf("==========================================\n");
-  printf("==========================================\n");
-  printf("==========================================\n");
-
-  ent = fopen("../src/e2", "r");
-  while(fgets(buff2, 1024, ent))
-  {
-    printf("%s", buff2);
-  }
-
-  fclose(ent);
-
-  return 0;
+  return 1;
 }
